6f5a9cab23a13c29feb918d1661d0e639f047085
victorm-fj <victorm.floresj@gmail.com>
2017-01-15 20:25:39 -0600

    Move styles to its own file

---
+++ app/styles.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 15,
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  author: {
+    fontSize: 20,
+    color: '#f8f8f8',
+    textAlign: 'right',
+    fontStyle: 'italic',
+    marginRight: 10,
+  },
+  quote: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginVertical: 30,
+  },
+  btn: {
+    width: 150,
+    padding: 15,
+    borderRadius: 5,
+    borderColor: '#fff',
+    borderWidth: 2,
+    marginBottom: 15,
+  },
+  text: {
+    fontSize: 18,
+    color: '#fff',
+    textAlign: 'center',
+  },
+});
+
+export default styles;
